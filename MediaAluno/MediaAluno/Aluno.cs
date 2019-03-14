de4f8a44ab4ba3a789005ffca201466c9918ff18
Mateus Martinez Rosa <mateusrttim@hotmail.com>
2019-03-14 15:25:20 -0300

    Meida aluno

---
+++ MediaAluno/MediaAluno/Aluno.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaAluno
+{
+    class Aluno
+    {
+        private string _nome;
+        private double _nota1;
+        private double _nota2;
+        private double _nota3;
+        
+        public string Nome { get => _nome; set => _nome = value; }
+        public double Nota1 { get => _nota1; set => _nota1 = value; }
+        public double Nota2 { get => _nota2; set => _nota2 = value; }
+        public double Nota3 { get => _nota3; set => _nota3 = value; }
+
+        //metodo que faz calcula da media do aluno
+        public double VerificaMedia()
+        {
+            return (this.Nota1 + this.Nota2 + this.Nota3) / 3;
+        }
+    }
+}
---
+++ MediaAluno/MediaAluno/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaAluno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno = new Aluno();
+            string entrada = "";
+
+            Console.WriteLine("___Calcula a média de uma aluno___");
+            Console.WriteLine();
+
+            Console.WriteLine("Informe seu nome: ");
+            entrada = Console.ReadLine();
+            aluno.Nome = entrada;
+
+            Console.WriteLine();
+
+            Console.WriteLine("Informe a nota 1: ");
+            entrada = Console.ReadLine();
+            aluno.Nota1 = Convert.ToDouble(entrada);
+
+            Console.WriteLine("Informe a nota 2: ");
+            entrada = Console.ReadLine();
+            aluno.Nota2 = Convert.ToDouble(entrada);
+
+            Console.WriteLine("Informe a nota 3: ");
+            entrada = Console.ReadLine();
+            aluno.Nota3 = Convert.ToDouble(entrada);
+
+            Console.WriteLine("A média do " + aluno.Nome + " é: " + aluno.VerificaMedia());
+
+            Console.ReadKey();
+        }
+    }
+}
